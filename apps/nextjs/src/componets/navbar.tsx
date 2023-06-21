e047b994087b358b9785b6c6d7eb500edfbb5098
vallen97 <42785044+vallen97@users.noreply.github.com>
2023-06-21 12:28:48 -0600

    Added NavBar
    Added a dynamic Navbar.

---
+++ apps/nextjs/src/componets/navbar.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+interface NavbarProps {
+  links: Array<string>;
+  linkNames: Array<string>;
+}
+
+export const Navbar: React.FC<NavbarProps> = (props) => {
+  return (
+    <>
+      <nav className="fixed top-0 left-0 z-20 w-full border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-900">
+        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+          <a href="https://flowbite.com/" className="flex items-center">
+            <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </a>
+          <div className="flex md:order-2">
+            <button
+              type="button"
+              className="mr-3 rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
+            >
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="h-6 w-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
+            id="navbar-sticky"
+          >
+            <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+              <>
+                {props.links.map((urls: string, index: number) => {
+                  return (
+                    <li>
+                      <a
+                        href={props.links[index]}
+                        className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                        aria-current="page"
+                      >
+                        {props.linkNames[index]}
+                      </a>
+                    </li>
+                  );
+                })}
+              </>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -3,10 +3,15 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
+import Navbar from "../componets/navbar";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Navbar
+        links={["/", "/employees", "/organization"]}
+        linkNames={["Home", "Employees", "Organization"]}
+      />
       <Component {...pageProps} />
     </ClerkProvider>
   );
